994ea5565286ae99f37655513bf5a99c3030c9b9
Zeinab8408 <zeinab.dehghan.84@outlook.com>
2025-01-20 21:32:57 -0800

    Update HyperPNN.py

--- Fusion-based/HyperPNN.py
+++ Fusion-based/HyperPNN.py
@@ -1,24 +1,17 @@
 ### HyperPNN: Hyperspectral Pansharpening via Spectrally Predictive Convolutional Neural Networks ###
 ### doi: 10.1109/JSTARS.2019.2917584 ###
 
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-
 class HyperPnn(nn.Module):
-    def __init__(self,hs_channels):
-        super(HyperPNN, self).__init__()
-        self.HSchannels   = hs_channels
-        self.PANchannels  = 1
-        self.mid_channels = 64
-        self.conv1 = nn.Conv2d(in_channels=self.HSchannels, out_channels=self.mid_channels, kernel_size=1)
-        self.conv2 = nn.Conv2d(in_channels=self.mid_channels, out_channels=self.mid_channels, kernel_size=1)
-        self.conv3 = nn.Conv2d(in_channels=self.mid_channels + self.PANchannels, out_channels=self.mid_channels, kernel_size=3, padding=1, stride=1)
-        self.conv4 = nn.Conv2d(in_channels=self.mid_channels, out_channels=self.mid_channels, kernel_size=3, padding=1, stride=1)
-        self.conv5 = nn.Conv2d(in_channels=self.mid_channels, out_channels=self.mid_channels, kernel_size=3, padding=1, stride=1)
-        self.conv6 = nn.Conv2d(in_channels=self.mid_channels, out_channels=self.mid_channels, kernel_size=1)
-        self.conv7 = nn.Conv2d(in_channels=self.mid_channels, out_channels=self.HSchannels, kernel_size=1)
+    def __init__(self,hs_channels, PAN_channels):
+        super(HyperPnn, self).__init__()
+        mid_channels = 64
+        self.conv1 = nn.Conv2d(hs_channels, mid_channels, kernel_size=1)
+        self.conv2 = nn.Conv2d(mid_channels, mid_channels, kernel_size=1)
+        self.conv3 = nn.Conv2d(mid_channels + PAN_channels, mid_channels, kernel_size=3, padding=1, stride=1)
+        self.conv4 = nn.Conv2d(mid_channels, mid_channels, kernel_size=3, padding=1, stride=1)
+        self.conv5 = nn.Conv2d(mid_channels, mid_channels, kernel_size=3, padding=1, stride=1)
+        self.conv6 = nn.Conv2d(mid_channels, mid_channels, kernel_size=1)
+        self.conv7 = nn.Conv2d(mid_channels, hs_channels, kernel_size=1)
         self.Sigmoid = nn.Sigmoid()
 
     def forward(self, LRHS,PAN): # LRHS-Size = [1,HS_channels,width,height],  PAN-size = [1,1,width,height]
@@ -34,9 +27,9 @@ class HyperPnn(nn.Module):
         x =  x + HRHS
         return x
 
-net = HyperPnn(hs_channels).to(device)
+net = HyperPnn(hs_channels, PAN_channels).to(device)
 optimizer = torch.optim.Adam(net.parameters(), lr=0.0001) # weight_decay=0
 
-def loss_fn(x,y):
+def lossFn(x,y):
     loss = torch.norm((y-x), p="fro")
     return loss
